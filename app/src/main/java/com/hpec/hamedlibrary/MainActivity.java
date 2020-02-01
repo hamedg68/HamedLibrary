d01371b44592252af3279280b923bad93c1743db
hamed gorji <user8@example.com>
2020-02-01 11:51:02 +0330

    Initial commit

---
+++ app/src/main/java/com/hpec/hamedlibrary/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hpec.hamedlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
